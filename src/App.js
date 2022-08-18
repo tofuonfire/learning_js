8d2405e72a1de1d6c6ed8ed0e2e0b04f05ab4965
NT <47516714+tofuonfire@users.noreply.github.com>
2022-08-18 15:13:23 +0900

    Add the functionality to add todo items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-console.log("App.js: loaded");
+import { element, render } from "./view/html-util.js";
+
 export class App {
-    constructor() {
-        console.log("App initialized");
+    mount() {
+        const formElement = document.querySelector("#js-form");
+        const inputElement = document.querySelector("#js-form-input");
+        const containerElement = document.querySelector("#js-todo-list");
+        const todoItemCountElement = document.querySelector("#js-todo-count");
+        // TodoリストをまとめるList要素
+        const todoListElement = element`<ul />`;
+        // Todoアイテム数
+        let todoItemCount = 0;
+        formElement.addEventListener("submit", (event) => {
+            // 本来のsubmitイベントの動作を止める
+            event.preventDefault();
+            // 追加するTodoアイテムの要素(li要素)を作成する
+            const todoItemElement = element`<li>${inputElement.value}</li>`;
+            // TodoアイテムをtodoListElementに追加する
+            todoListElement.appendChild(todoItemElement);
+            // コンテナ要素の中身をTodoリストをまとめるList要素で上書きする
+            render(todoListElement, containerElement);
+            // Todoアイテム数を+1し、表示されてるテキストを更新する
+            todoItemCount += 1;
+            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+            // 入力欄を空文字列にしてリセットする
+            inputElement.value = "";
+        });
     }
 }
