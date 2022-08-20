5cdae26b771781f8f57b74f30b4734613fe6cf81
NT <47516714+tofuonfire@users.noreply.github.com>
2022-08-20 11:10:47 +0900

    Add the feature to update/delete todo items

--- src/model/TodoListModel.js
+++ src/model/TodoListModel.js
@@ -49,4 +49,34 @@ export class TodoListModel extends EventEmitter {
         this.#items.push(todoItem);
         this.emitChange();
     }
+
+    /**
+     * 指定した`id`のTodoItemの`completed`を更新する
+     * @param {{ id:number, completed: boolean }}
+     */
+    updateTodo({ id, completed }) {
+        const todoItem = this.#items.find(todo => todo.id === id);
+        if (!todoItem) {
+            return;
+        }
+        todoItem.completed = completed;
+        this.emitChange();
+    }
+
+    //! [add-point]
+    // ===============================
+    // TodoListModel.jsの既存の実装は省略
+    // ===============================
+    /**
+     * 指定したidのTodoItemを削除する
+     * @param {{ id: number }}
+     */
+    deleteTodo({ id }) {
+        // `id`に一致しないTodoItemだけを残すことで、`id`に一致するTodoItemを削除する
+        this.#items = this.#items.filter(todo => {
+            return todo.id !== id;
+        });
+        this.emitChange();
+    }
 }
+//! [add-point]
